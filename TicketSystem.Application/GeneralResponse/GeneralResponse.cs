7797436e575755c7bb5ee286736cb27696f06aba
moudy99 <bobmoudy1@gmail.com>
2024-08-07 13:19:12 +0300

    add GeneralResponse

---
+++ TicketSystem.Application/GeneralResponse/GeneralResponse.cs
@@ -0,0 +1,13 @@
+﻿using TicketSystem.Application.DTOs;
+
+namespace TicketSystem.Application.GeneralResponse
+{
+    public class GeneralResponse<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public bool Succeeded { get; set; }
+        public List<string> Errors { get; set; }
+        public PaginationInfoDTO PaginationInfo { get; set; }
+    }
+}
